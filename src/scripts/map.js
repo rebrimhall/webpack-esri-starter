e1703ddce057c2b4d4fdbbcb9b0bb8554df35410
Robert <rebrimhall@gmail.com>
2019-08-22 15:25:26 -0500

    update

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -4,11 +4,20 @@ import { loadCss } from 'esri-loader';
 // if the API hasn't already been loaded (i.e. the frist time this is run)
 // loadModules() will call loadScript() and pass these options, which,
 // in this case are only needed b/c we're using v3.x instead of the latest 4.x
-const options = { version: '3.28' };
-loadCss('3.28');
+const options = {}; //{ version: '3.28' };
+loadCss(); //('3.28');
 
-loadModules(['esri/map', 'esri/geometry/Extent'], options)
-  .then(([Map, Extent]) => {
+loadModules(
+  [
+    'esri/views/MapView',
+    'esri/views/SceneView',
+    'esri/WebMap',
+    'esri/layers/TileLayer',
+    'esri/geometry/Extent'
+  ],
+  options
+)
+  .then(([MapView, SceneView, WebMap, TileLayer, Extent]) => {
     const wkid = 3857;
 
     const USExtent = new Extent({
@@ -24,19 +33,36 @@ loadModules(['esri/map', 'esri/geometry/Extent'], options)
         wkid: wkid
       }
     });
-    // create map with the given options at a DOM node w/ id 'mapNode'
-    let map = new Map('mapNode', {
-      sliderPosition: 'top-right',
-      basemap: 'streets',
-      center: [-98.579416, 39.828328],
-      zoom: 5,
-      scrollWheelZoom: true,
-      sliderPosition: 'top-right',
-      logo: false,
-      isKeyboardNavigation: false,
-      navigationMode: 'css-transforms',
-      extent: USExtent
+
+    var transportationLayer = new TileLayer({
+      url:
+        'https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer',
+      id: 'streets',
+      opacity: 0.7
     });
+
+    // create map with the given options at a DOM node w/ id 'mapNode'
+    let map = new WebMap({
+      basemap: 'oceans',
+      // ground: 'world-elevation',
+      layers: [transportationLayer]
+    });
+
+    let view = new MapView({
+      container: 'mapNode',
+      map: map,
+      center: [-98.579416, 39.828328], // longitude, latitude
+      zoom: 4
+    });
+
+    /*let view = new SceneView({
+      container: 'mapNode',
+      map: map,
+      center: [-98.579416, 39.828328], // longitude, latitude
+      zoom: 5
+    });*/
+
+    view.ui._removeComponents(['attribution']);
 
     /*fetch(
       'urlhere'
